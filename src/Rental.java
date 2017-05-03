8d6cdbba5ebbcb503d213f95fae14084def4e258
Christian Gutermann <christian.gutermann1337@gmail.com>
2017-05-03 09:56:45 +0200

    moved method getCharge from customer to rental

--- src/Rental.java
+++ src/Rental.java
@@ -13,12 +13,22 @@ class Rental {
     }
 
     public double getCharge() {
-        return getMovie().getCharge(getDaysRented());
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return result;
     }
-
-    public int getFrequentRenterPoints() {
-        return getMovie().getFrequentRenterPoints(getDaysRented());
-    }
-
-
 }
