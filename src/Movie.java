2cdf38f2c31fdb4854494464b681fc28de52b521
Christian Gutermann <christian.gutermann1337@gmail.com>
2017-05-03 10:35:06 +0200

    Fixed some errors

--- src/Movie.java
+++ src/Movie.java
@@ -4,6 +4,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String title;
     private Price _price;
+    
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         setPriceCode(newpriceCode);
@@ -30,31 +31,10 @@ public class Movie {
         return title;
     }
     double getCharge(int daysRented) {
-    	double result = 0;
-    	switch (getPriceCode()) {
-    	 case Movie.REGULAR:
-    	 result += 2;
-    	 if (daysRented > 2)
-    		 result += (daysRented - 2) * 1.5;
-    	 break;
-    	 case Movie.NEW_RELEASE:
-    		 result += daysRented * 3;
-    	 break;
-    	 case Movie.CHILDRENS:
-    		 result += 1.5;
-    		 if (daysRented > 3)
-    			 result += (daysRented - 3) * 1.5;
-    	 break;
-    	 }
-    	return result;
+    	return _price.getCharge(daysRented);
     }
     public int getFrequentRenterPoints(int daysRented){
-    	if((getPriceCode()== Movie.NEW_RELEASE)&& daysRented>1){
-    		return 2;
-    	}
-    	else{
-    		return 1;
-    	}
+    	return _price.getFrequentRenterPoints(daysRented);
     }
     
 }
--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -1,22 +1,14 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Berkling
- * Date: 14.05.14
- * Time: 12:49
- * To change this template use File | Settings | File Templates.
- */
-public class NewReleasePrice implements Price {
-    public int getPriceCode(){
-        return Movie.NEW_RELEASE;
-    }
 
-    public double getCharge(int daysRented){
-        return (daysRented * 3 );
-    }
+public class NewReleasePrice extends Price {
 
-    public int getFrequentRenterPoints(int daysRented) {
-        return 2;
-    }
-
+	@Override
+	int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+	double getCharge(int daysRented){
+		 return daysRented * 3;
+	}
+	int getFrequentRenterPoints(int daysRented) {
+		 return (daysRented > 1) ? 2: 1;
+	}
 }
-
