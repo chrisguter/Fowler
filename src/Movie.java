d39d4abcdb3c828ad2a2f3ccf36f401cce3980c0
Christian Gutermann <christian.gutermann1337@gmail.com>
2017-05-03 10:28:05 +0200

    Replace Type Code with State/strategy

--- src/Movie.java
+++ src/Movie.java
@@ -3,46 +3,58 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
-    private Price price;
-
-    public Movie(String newtitle, int priceCode) {
+    private Price _price;
+    public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        setPriceCode(priceCode);
+        setPriceCode(newpriceCode);
     }
-
     public int getPriceCode() {
-        return price.getPriceCode();
+        return _price.getPriceCode();
     }
-
     public void setPriceCode(int arg) {
-        switch (arg) {
-            case REGULAR:
-                price = new RegularPrice();
-                break;
-            case CHILDRENS:
-                price = new ChildrensPrice();
-                break;
-            case NEW_RELEASE:
-                price = new NewReleasePrice();
-                break;
-            default:
-                throw new IllegalArgumentException("Incorrect Price Code");
+        switch (arg){
+        	case REGULAR:
+        		_price = new RegularPrice();
+        		break;
+        	case CHILDRENS:
+        		_price = new ChildrensPrice();
+        		break;
+        	case NEW_RELEASE:
+        		_price = new NewReleasePrice();
+        		break;
+        	default:
+        		throw new IllegalArgumentException("Incorrect Price Code");
         }
     }
-
     public String getTitle (){
         return title;
     }
-
-    public double getCharge(int daysRented) {
-        return price.getCharge(daysRented);
+    double getCharge(int daysRented) {
+    	double result = 0;
+    	switch (getPriceCode()) {
+    	 case Movie.REGULAR:
+    	 result += 2;
+    	 if (daysRented > 2)
+    		 result += (daysRented - 2) * 1.5;
+    	 break;
+    	 case Movie.NEW_RELEASE:
+    		 result += daysRented * 3;
+    	 break;
+    	 case Movie.CHILDRENS:
+    		 result += 1.5;
+    		 if (daysRented > 3)
+    			 result += (daysRented - 3) * 1.5;
+    	 break;
+    	 }
+    	return result;
     }
-
-    public int getFrequentRenterPoints( int daysRented) {
-        return price.getFrequentRenterPoints(daysRented);
-
+    public int getFrequentRenterPoints(int daysRented){
+    	if((getPriceCode()== Movie.NEW_RELEASE)&& daysRented>1){
+    		return 2;
+    	}
+    	else{
+    		return 1;
+    	}
     }
-
-
+    
 }
