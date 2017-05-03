6059a98ce1115e38ceefe74a521ffb798b2d7603
Christian Gutermann <christian.gutermann1337@gmail.com>
2017-05-03 10:05:12 +0200

    Changed location getCharge to Movie

--- src/Movie.java
+++ src/Movie.java
@@ -4,45 +4,37 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String title;
     private int priceCode;
-    private Price price;
-
-    public Movie(String newtitle, int priceCode) {
+    public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        setPriceCode(priceCode);
+        priceCode = newpriceCode;
     }
-
     public int getPriceCode() {
-        return price.getPriceCode();
+        return priceCode;
     }
-
     public void setPriceCode(int arg) {
-        switch (arg) {
-            case REGULAR:
-                price = new RegularPrice();
-                break;
-            case CHILDRENS:
-                price = new ChildrensPrice();
-                break;
-            case NEW_RELEASE:
-                price = new NewReleasePrice();
-                break;
-            default:
-                throw new IllegalArgumentException("Incorrect Price Code");
-        }
+        priceCode = arg;
     }
-
     public String getTitle (){
         return title;
     }
 
-    public double getCharge(int daysRented) {
-        return price.getCharge(daysRented);
+    public double getCharge(int days) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if ( days > 2)
+                    result += (days - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += days * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (days > 3)
+                    result += (days - 3) * 1.5;
+                break;
+        }
+        return result;
     }
-
-    public int getFrequentRenterPoints( int daysRented) {
-        return price.getFrequentRenterPoints(daysRented);
-
-    }
-
-
 }
